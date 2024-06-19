9d80e253f21114a543525876e1a2fc304775bf99
jareb Smith <jareb.smith@odmhsas.org>
2024-06-19 17:13:07 -0500

    Create Products init

---
+++ Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    //private readonly IProductRepository _productRepository;
+
+    //public CreateProductHandler(IProductRepository productRepository)
+    //{
+    //    _productRepository = productRepository;
+    //}
+
+    //public async Task<Guid> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    //{
+    //    var product = new Product
+    //    {
+    //        Name = request.Name,
+    //        Categories = request.Categories,
+    //        Description = request.Description,
+    //        ImageFile = request.ImageFile,
+    //        Price = request.Price
+    //    };
+
+    //    await _productRepository.AddProductAsync(product, cancellationToken);
+
+    //    return product.Id;
+    //}
+
+    Task<CreateProductResult> IRequestHandler<CreateProductCommand, CreateProductResult>.Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        // Business logic to create a product
+        throw new NotImplementedException();
+    }
+}
