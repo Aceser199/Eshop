ee42db73aeb763cc3c318a321c4a83726d8ca4af
jareb Smith <jareb.smith@odmhsas.org>
2024-06-20 19:06:54 -0500

    Pagination

--- Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,15 +1,15 @@
 ﻿namespace Catalog.API.Products.GetProducts;
 
-//public record GetProductRequest();
+public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
 public record class GetProductResponse(IEnumerable<Product> Products);
 
 public class GetProductsEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender) =>
+        app.MapGet("/products", async ([AsParameters] GetProductsRequest request, ISender sender) =>
         {
-            var query = new GetProductsQuery();
+            var query = request.Adapt<GetProductsQuery>();
             var result = await sender.Send(query, default);
 
             var response = result.Adapt<GetProductResponse>();
